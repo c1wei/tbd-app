beed252f6d5be26e6a1d3d96d1f9e18c83e07f37
c1wei <c1wei@ucsd.edu>
2017-04-19 12:54:05 -0700

    isValidDate implementation. provided tests as well. TODO, test more extensively with invalid input

--- test/test.js
+++ test/test.js
@@ -22,3 +22,35 @@ describe('Foo', function() {
     });
   })
 });
+
+describe('IsValidDate', function() {
+  describe('#isValidDate()', function() {
+    it('should return back true if passed a valid date', function() {
+      var foo = require('../date/date');
+      assert.equal(foo.isValidDate('10-12-1961'), true);
+      assert.equal(foo.isValidDate('12/11/1961'), true);
+
+    });
+
+    it('should return back false if not a valid date', function() {
+      var foo = require('../date/date');
+      assert.equal(foo.isValidDate('11-31-1061'), false);
+      assert.equal(foo.isValidDate('13-11-1961'), false);
+    });
+  })
+});
+
+describe('date2', function() {
+  describe('#date2()', function() {
+    it('should return back true if passed date', function() {
+      var foo = require('../date/date');
+      assert.equal(foo.date2('date2'), true);
+    });
+
+    it('should return back false if passed something other than date', function() {
+      var foo = require('../date/date');
+      assert.equal(foo.date2('foo'), false);
+      assert.equal(foo.date2('bar'), false);
+    });
+  })
+});
