1fcbda8984c21a6394d447227c2e45ee8b632ae3
John Gamboa <johnhadriansgamboa@gmail.com>
2017-04-20 14:39:04 -0700

    configure hello route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var hello = require('./hello');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/hello', hello);
 
 module.exports = router;
